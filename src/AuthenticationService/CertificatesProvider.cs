58f34f1bd3aae90407b7b79461852a9282a2ccd0
Stefano Cocchi <stefano.cocchi.it@gmail.com>
2019-06-22 19:21:23 +0100

    Improved logging

--- src/AuthenticationService/CertificatesProvider.cs
+++ src/AuthenticationService/CertificatesProvider.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
@@ -16,8 +17,16 @@ namespace AuthenticationService
 
         public async Task<X509Certificate2> GetCertificateAsync(string certificateName, string secretName)
         {
-            var password = await this.secretsStore.GetSecret(secretName);
-            var certificate = await this.certificatesStore.GetCertificateAsync(certificateName, password);
+            X509Certificate2 certificate;
+            try
+            {
+                var password = await this.secretsStore.GetSecret(secretName);
+                certificate = await this.certificatesStore.GetCertificateAsync(certificateName, password);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception($"Could not get certificate \"{certificateName}\". See inner exception.", exception);
+            }
 
             return certificate;
         }
