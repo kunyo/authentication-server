e23078b03c783fe0b0fc05770dbf08e0c4e8178d
Stefano Cocchi <stefano.cocchi.it@gmail.com>
2019-06-22 18:12:52 +0100

    Include environment configuration into docker image

--- push.sh
+++ push.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 set -e
 
-AWS_PROFILE="eimpresa-automation"
-AWS_REGION="eu-west-1"
+TARGET_ENV="$1"
 ECR_REPOSITORY_URI="353153972523.dkr.ecr.eu-west-1.amazonaws.com/authentication-server-dev"
-ECS_CLUSTER_NAME="authentication-server-dev"
-ECS_SERVICE_NAME="authentication-server-dev"
+ECS_CLUSTER_NAME="authentication-server-$TARGET_ENV"
+ECS_SERVICE_NAME="authentication-server-$TARGET_ENV"
 DOCKER_IMAGE_NAME="authentication-service:latest"
+APP_CONFIGURATION_FILE="$(pwd)/config/$TARGET_ENV/appsettings.json"
+
+test -n "$TARGET_ENV" || (>&2 echo "Usage: $0 <environment name>"; exit 1)
+test -n "$AWS_PROFILE" || (>&2 echo "AWS_PROFILE environment variable must be defined."; exit 1)
+test -n "$AWS_REGION" || (>&2 echo "AWS_REGION environment variable must be defined."; exit 1)
 
 # Get the docker image id
 DOCKER_IMAGE_ID="$(docker images -q $DOCKER_IMAGE_NAME)"
-test -n "$DOCKER_IMAGE_ID" || (echo "Docker image not found on local repository: $DOCKER_IMAGE_NAME")
+test -n "$DOCKER_IMAGE_ID" || (>&2 echo "Docker image not found on local repository: $DOCKER_IMAGE_NAME")
+test -f "$APP_CONFIGURATION_FILE" || (>&2 echo "App configuration file not found: $APP_CONFIGURATION_FILE")
 
 echo "aws_profile = $AWS_PROFILE"
 echo "aws_region = $AWS_REGION"
@@ -19,40 +24,32 @@ echo "ecs_cluster_name = $ECS_CLUSTER_NAME"
 echo "ecs_service_name = $ECS_SERVICE_NAME"
 echo "docker_image_name = $DOCKER_IMAGE_NAME"
 echo "docker_image_id = $DOCKER_IMAGE_ID"
+echo "app_configuration_file = $APP_CONFIGURATION_FILE"
 echo
 read -p "Is this OK? (Yes): " force
 [ "$force" = "Yes" ] || (echo "Nothing done, exiting..."; exit 1)
 
+# Creates the deployment image
+TMP_CONTAINER_ID=$(docker create $DOCKER_IMAGE_ID)
+
+# Copy the application configuration to the app directory specified in the DockerFile
+docker cp "$APP_CONFIGURATION_FILE" "$TMP_CONTAINER_ID:/app/appsettings.json"
+
+# Commit the temporary container and tag it with the target repository uri
+docker commit $TMP_CONTAINER_ID "$ECR_REPOSITORY_URI:latest"
+
 # Get the login command for the ECR Repository
 echo "Getting ECR repository credential"
 DOCKER_LOGIN_CMD="$(aws ecr get-login --profile $AWS_PROFILE --region $AWS_REGION --no-include-email)"
-
-# Tag the image with the repository url
-echo "Tagging repository"
-docker tag "$DOCKER_IMAGE_ID" "$ECR_REPOSITORY_URI:latest"
 
 # Push the image to the repository
 echo "Pushing the image to ECR"
 $DOCKER_LOGIN_CMD
 docker push "$ECR_REPOSITORY_URI"
 
-# Remove the image copy created by the taggin operation
+# Remove the temporary image and container
+docker rm "$TMP_CONTAINER_ID"
 docker rmi "$ECR_REPOSITORY_URI:latest"
-
-# Stop running tasks
-# tasks=$(\
-#     aws ecs list-tasks \
-#     --profile "$AWS_PROFILE" --region "$AWS_REGION" \
-#     --cluster "$ECS_CLUSTER_NAME" --service "$ECS_SERVICE_NAME" \
-#     | jq -r '.taskArns[]'\
-# )
-
-# for i in $tasks; do 
-#     echo "Stopping task $i"; 
-#     aws ecs stop-task \
-#     --profile "$AWS_PROFILE" --region "$AWS_REGION" \
-#     --cluster "$ECS_CLUSTER_NAME" --task "$i"; 
-# done
 
 # Run task
 echo "Forcing service redeployment"
